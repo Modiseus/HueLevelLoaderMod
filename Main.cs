ac65e49016b46c833845ad83bbd51dff4500ebcd
Jan Eich <j.eich@keiro.ch>
2020-07-18 00:04:14 +0200

    Rename to LevelLoaderMod. Add Prepare For NG+ button.

--- Main.cs
+++ Main.cs
@@ -5,12 +5,11 @@ using System.Text;
 using UnityEngine;
 using UnityModManagerNet;
 
-namespace LevelLoader
+namespace LevelLoaderMod
 {
     public class Main
     {
 
-        private static bool visible;
         private static Vector2 scrollPosition;
 
 
@@ -22,46 +21,148 @@ namespace LevelLoader
 
         private static void FixedGUI(UnityModManager.ModEntry modEntry)
         {
-            GUILayout.BeginVertical();
             
-            GUILayout.Label("Level Loader by Modiseus");
+
+            GUILayout.BeginVertical();
+
+            Texture2D texture = new Texture2D(1, 1);
+            texture.SetPixel(0, 0, Color.black);
+            texture.Apply();
+            GUIStyle style = new GUIStyle();
+
+            style.normal.background = texture;
+            style.normal.textColor = Color.white;
+
+            GUILayout.Label("Level Loader Mod Enabled", style);
+
+            GameManager gm = GameManager.instance;
+            if (gm == null)
+            {
+                GUILayout.EndVertical();
+                return;
+            }
+            PlayerNew player = gm.Player;
+            if(player == null)
+            {
+                GUILayout.EndVertical();
+                return;
+            }
+
 
             GUILayout.BeginHorizontal();
-            
+
             GUILevels(250, 500);
 
-            if(GUILayout.Button("Unlock All Colors"))
+            GUILayout.BeginVertical();
+
+            if (GUILayout.Button("Unlock Colors"))
             {
-                Singleton<SaveLoadManager>.Instance.SetColourUnlocked(HueColour.HueColorNames.Aqua, true);
-                Singleton<SaveLoadManager>.Instance.SetColourUnlocked(HueColour.HueColorNames.Blue, true);
-                Singleton<SaveLoadManager>.Instance.SetColourUnlocked(HueColour.HueColorNames.Lime, true);
-                Singleton<SaveLoadManager>.Instance.SetColourUnlocked(HueColour.HueColorNames.Orange, true);
-                Singleton<SaveLoadManager>.Instance.SetColourUnlocked(HueColour.HueColorNames.Pink, true);
-                Singleton<SaveLoadManager>.Instance.SetColourUnlocked(HueColour.HueColorNames.Purple, true);
-                Singleton<SaveLoadManager>.Instance.SetColourUnlocked(HueColour.HueColorNames.Red, true);
-                Singleton<SaveLoadManager>.Instance.SetColourUnlocked(HueColour.HueColorNames.Yellow, true);
+                UnlockColors();
             }
 
-            if(GUILayout.Button("Unlock Map"))
+            if (GUILayout.Button("Unlock Map"))
             {
-                foreach (Regions region in Enum.GetValues(typeof(Regions)))
-                {
-                    Singleton<SaveLoadManager>.Instance.UnlockRegion(region);
-                }
+                UnlockMap();
             }
 
-            if(GUILayout.Button("Unlock Events"))
+            if (GUILayout.Button("Unlock Events"))
             {
-                foreach (EventTrigger eventTrigger in Enum.GetValues(typeof(EventTrigger)))
-                {
-                    
-                    Singleton<SaveLoadManager>.Instance.TriggerEvent(eventTrigger);
-                }
+                UnlockEvents();
             }
+
+            if(GUILayout.Button("Complete Tutorials"))
+            {
+                CompleteTutorials();
+            }
+
+            if (GUILayout.Button("Prepare For NG+"))
+            {
+                Singleton<SaveLoadManager>.Instance.NewGame();
+                UnlockColors();
+                UnlockMap();
+                UnlockEvents();
+                CompleteTutorials();
+                LoadLevel("OldLadyHouse");
+            }
+
+            GUILayout.EndVertical();
 
             GUILayout.EndHorizontal();
 
             GUILayout.EndVertical();
+
+
+        }
+
+        private static void UnlockColors()
+        {
+            Singleton<SaveLoadManager>.Instance.SetColourUnlocked(HueColour.HueColorNames.Aqua, true);
+            Singleton<SaveLoadManager>.Instance.SetColourUnlocked(HueColour.HueColorNames.Blue, true);
+            Singleton<SaveLoadManager>.Instance.SetColourUnlocked(HueColour.HueColorNames.Lime, true);
+            Singleton<SaveLoadManager>.Instance.SetColourUnlocked(HueColour.HueColorNames.Orange, true);
+            Singleton<SaveLoadManager>.Instance.SetColourUnlocked(HueColour.HueColorNames.Pink, true);
+            Singleton<SaveLoadManager>.Instance.SetColourUnlocked(HueColour.HueColorNames.Purple, true);
+            Singleton<SaveLoadManager>.Instance.SetColourUnlocked(HueColour.HueColorNames.Red, true);
+            Singleton<SaveLoadManager>.Instance.SetColourUnlocked(HueColour.HueColorNames.Yellow, true);
+        }
+
+        private static void UnlockMap()
+        {
+            foreach (Regions region in Enum.GetValues(typeof(Regions)))
+            {
+                Singleton<SaveLoadManager>.Instance.UnlockRegion(region);
+            }
+        }
+
+        private static void UnlockEvents()
+        {
+            Singleton<SaveLoadManager>.Instance.TriggerEvent(EventTrigger.EnteredVillage);
+            Singleton<SaveLoadManager>.Instance.TriggerEvent(EventTrigger.FreedMiner);
+            Singleton<SaveLoadManager>.Instance.TriggerEvent(EventTrigger.DrGreySpeak);
+            Singleton<SaveLoadManager>.Instance.TriggerEvent(EventTrigger.DrGreyDisappear);
+            Singleton<SaveLoadManager>.Instance.TriggerEvent(EventTrigger.SpokenToMiner);
+            Singleton<SaveLoadManager>.Instance.TriggerEvent(EventTrigger.CanUseColourWheel);
+            Singleton<SaveLoadManager>.Instance.TriggerEvent(EventTrigger.SpokenToLighthouseKeeper);
+            Singleton<SaveLoadManager>.Instance.TriggerEvent(EventTrigger.Voiceover1);
+            Singleton<SaveLoadManager>.Instance.TriggerEvent(EventTrigger.TurnOnLighthouse);
+            Singleton<SaveLoadManager>.Instance.TriggerEvent(EventTrigger.VOIntro);
+            Singleton<SaveLoadManager>.Instance.TriggerEvent(EventTrigger.ColourwheelTutorial);
+            Singleton<SaveLoadManager>.Instance.TriggerEvent(EventTrigger.DrGreyVillageLetter);
+            Singleton<SaveLoadManager>.Instance.TriggerEvent(EventTrigger.DrGreyOrange);
+            Singleton<SaveLoadManager>.Instance.TriggerEvent(EventTrigger.DrGreyPurple);
+            Singleton<SaveLoadManager>.Instance.TriggerEvent(EventTrigger.DrGreyPink);
+            Singleton<SaveLoadManager>.Instance.TriggerEvent(EventTrigger.DrGreyRed);
+            Singleton<SaveLoadManager>.Instance.TriggerEvent(EventTrigger.DrGreyBlue);
+            Singleton<SaveLoadManager>.Instance.TriggerEvent(EventTrigger.DrGreyYellow);
+            Singleton<SaveLoadManager>.Instance.TriggerEvent(EventTrigger.DrGreyLime);
+            Singleton<SaveLoadManager>.Instance.TriggerEvent(EventTrigger.DrGreyMountains);
+            Singleton<SaveLoadManager>.Instance.TriggerEvent(EventTrigger.DrGreyRoofTalk);
+            Singleton<SaveLoadManager>.Instance.TriggerEvent(EventTrigger.DrGreyUniStart);
+            Singleton<SaveLoadManager>.Instance.TriggerEvent(EventTrigger.DrGreyLastLevel);
+            Singleton<SaveLoadManager>.Instance.TriggerEvent(EventTrigger.DrGreyRoofFall);
+            Singleton<SaveLoadManager>.Instance.TriggerEvent(EventTrigger.GameComplete);
+            Singleton<SaveLoadManager>.Instance.TriggerEvent(EventTrigger.GameCompleteGotOnBoat);
+            Singleton<SaveLoadManager>.Instance.TriggerEvent(EventTrigger.GameCompleteReturnedToHouse);
+            Singleton<SaveLoadManager>.Instance.TriggerEvent(EventTrigger.GameCompleteDream);
+            Singleton<SaveLoadManager>.Instance.TriggerEvent(EventTrigger.CreditsRolled);
+            Singleton<SaveLoadManager>.Instance.TriggerEvent(EventTrigger.PostCreditsEnteredVillage);
+        }
+
+        private static void CompleteTutorials()
+        {
+            Singleton<SaveLoadManager>.Instance.CompleteTutorial(TutorialType.ColourWheel);
+            Singleton<SaveLoadManager>.Instance.CompleteTutorial(TutorialType.EnterDoor);
+            Singleton<SaveLoadManager>.Instance.CompleteTutorial(TutorialType.JumpDown);
+            Singleton<SaveLoadManager>.Instance.CompleteTutorial(TutorialType.Pull);
+            Singleton<SaveLoadManager>.Instance.CompleteTutorial(TutorialType.Talk);                       
+        }
+
+        private static void LoadLevel(String sceneName)
+        {
+            GameManager.instance.HueSceneManager.SaveSceneInfo(Vector3.zero, Vector3.zero, -1, Vector2.zero);
+            UnityEngine.Object.Destroy(GameManager.instance.Player.gameObject);
+            Singleton<SaveLoadManager>.Instance.Checkpoint(sceneName, -1, Vector3.zero);
+            LoadingComponent.Instance.LoadNextLevel();
         }
 
         private static void GUILevels(int width, int height)
@@ -71,15 +172,10 @@ namespace LevelLoader
             var levelList = GameManager.instance.GetLevelNames();
             foreach (string name in levelList)
             {
+
                 if (GUILayout.Button(name))
                 {
-                    String sceneName = name;
-
-
-                    GameManager.instance.HueSceneManager.SaveSceneInfo(Vector3.zero, Vector3.zero, -1, Vector2.zero);
-                    UnityEngine.Object.Destroy(GameManager.instance.Player.gameObject);
-                    Singleton<SaveLoadManager>.Instance.Checkpoint(sceneName, -1, Vector3.zero);
-                    LoadingComponent.Instance.LoadNextLevel();
+                    LoadLevel(name);
                 }
 
             }
@@ -87,5 +183,8 @@ namespace LevelLoader
             GUILayout.EndScrollView();
 
         }
+
+
+
     }
 }
